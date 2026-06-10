25f9ef4ef85babbfc851deb0ca6279fe90dc9cb6
TristanLimon <1.diegoflores.1@gmail.com>
2026-06-09 19:03:41 -0600

    Backend de LandingPage

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -1,5 +1,32 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "courses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	@Column(length = 2000)
+	private String description;
+
+	private String level;
+
+	private Double price;
+
+	private Boolean published;
+
+	private String imageUrl;
 
 }
